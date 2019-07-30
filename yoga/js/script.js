9a836aad15ec03860c433a289a1dbf5226c61480
Max <tranmod@inbox.ru>
2019-07-30 10:38:45 +0300

    AJAX

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -142,7 +142,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
     newDiv.newDiv();
 
-    
+    // Forms
 
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо. Скоро свяжемся',
+        failure: 'Что-то пошло не так!'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+
+        request.open('POST', 'server.php');
+        //request.setRequestHeader('Content-Type', 'application/x-ww-form-urlencoded');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        let formData = new FormData(form);
+
+        let obj = {};
+
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        //request.send(formData);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(event){
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+    });
 
 });
